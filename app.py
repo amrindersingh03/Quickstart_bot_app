170644e1471f79dfc7ccb04f1b25dfb5a24aa8b2
amrindersingh03 <117232889+amrindersingh03@users.noreply.github.com>
2023-06-07 02:01:43 -0300

    app.py

--- app.py
+++ app.py
@@ -1,18 +1,28 @@
 import streamlit as st
-from langchain.llms import OpenAI
+import openai
 
 st.title('First_app')
 
-openai_api_key = st.sidebar.text_input('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')
+openai_api_key = st.sidebar.text_input('OpenAI API Key')
 
 def generate_response(input_text):
-  llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
-  st.info(llm(input_text))
+    # Use the OpenAI API
+    response = openai.Completion.create(
+        engine='text-davinci-003',
+        prompt=input_text,
+        temperature=0.7,
+        max_tokens=100,
+        n = 1,
+        stop = None,
+        frequency_penalty = 0,
+        presence_penalty = 0
+    )
+    st.info(response.choices[0].text.strip())
 
 with st.form('my_form'):
-  text = st.text_area('Enter text:', 'What are the three key pieces of advice for learning how to code?')
-  submitted = st.form_submit_button('Submit')
-  if not openai_api_key.startswith('sk-'):
-    st.warning('Please enter your OpenAI API key!', icon='⚠')
-  if submitted and openai_api_key.startswith('sk-'):
-    generate_response(text)
+    text = st.text_area('Enter text:', 'What are the three key pieces of advice for learning how to code?')
+    submitted = st.form_submit_button('Submit')
+
+    if submitted and openai_api_key:
+        openai.api_key = openai_api_key
+        generate_response(text)
